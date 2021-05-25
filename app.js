5a866d66aeced0f6ea707d6b226a2d5fdd22efd3
leoazz <81827748+leoazz@users.noreply.github.com>
2021-05-25 21:11:58 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+
+window.fetch('https://api.openweathermap.org/data/2.5/onecall?lat=48.6833&lon=6.2167&exclude=minutely&lang=fr&units=metric&appid=47fdff73311a5451cc64f7ecee1df5e3')
+	.then(response => response.json())
+
+
